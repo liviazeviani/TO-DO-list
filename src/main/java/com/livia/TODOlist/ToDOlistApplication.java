b6fd609a5d6519db1d97d71d4a967022fe1434f8
liviazeviani <liviapavinizeviani@gmail.com>
2022-11-20 00:44:51 -0300

    adding files from project

---
+++ src/main/java/com/livia/TODOlist/ToDOlistApplication.java
@@ -0,0 +1,13 @@
+package com.livia.TODOlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDOlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDOlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/livia/TODOlist/ToDOlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livia.TODOlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDOlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
